04cab0502df595d4a5ebc74488eec1ab6ac9759c
Wyliemaster <62946885+Wyliemaster@users.noreply.github.com>
2020-10-28 17:12:50 +0000

    code

---
+++ README.md
@@ -0,0 +1,27 @@
+# Save Decoder/Encoder
+
+## what is needed?
+
+To Use this tool all you need is:
+
+- [Node.js](https://nodejs.org/en/) installed
+
+- Zlib and fs
+
+`npm install zlib --save`
+`npm install fs --save`
+
+- an IDE, [this is what i use](https://code.visualstudio.com/)
+
+## how to use
+
+using this tool is very simple, all you need to do is place the file you want to decode/encode in the same directory as index.js
+![example](https://media.discordapp.net/attachments/692142942856544288/771058089323266088/unknown.png)
+
+and then use the functions ive written
+
+````js
+  decodeSave(/*Please insert the name of your save file*/ )
+
+  encodeSave(/*Please insert the name of the file you want to encode*/ )```
+````
---
+++ index.js
@@ -0,0 +1,35 @@
+const zlib = require('zlib')
+const fs = require('fs')
+
+function Xor(key, string){
+        result = '';
+        for (var index = 0; index < string.length; index++) {
+          var input = string.charCodeAt(index);
+      result += String.fromCharCode(input ^ key);
+        }
+      return result;
+}
+
+function decodeSave(fileName){
+    data = fs.readFileSync(__dirname + '/' + fileName, "utf-8") //CCGameManager or CCLocalLevels
+    Xord = Xor(0xB, data)
+    base64 = Buffer.from(Xord, 'base64')
+    result = zlib.unzipSync(base64).toString()
+    fs.writeFileSync('decoded.xml', result)
+    return console.log('Your Save Data has been decoded')
+}
+
+function encodeSave(fileName) {
+  data = fs.readFileSync(__dirname + '/' + fileName, "utf8");
+  zipped = zlib.gzipSync(data, 'base64')
+  base64 = new Buffer.from(zipped, "utf-8").toString("base64")
+  done = Xor(0xB, base64)
+  fs.writeFileSync('CCGameManager2222.dat', done)
+  return console.log('Save File Generated')
+  }
+
+
+
+  decodeSave(/*Please insert the name of your save file*/ )
+
+  encodeSave(/*Please insert the name of the file you want to encode*/ )

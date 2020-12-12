358aecd6142d3f8e8cfe40ad3986fb6238d6eaac
Wyliemaster <62946885+Wyliemaster@users.noreply.github.com>
2020-12-12 15:10:15 +0000

    urlsafeb64
    commented out encode save as it doesn't work and i need to fix it still

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
 const zlib = require('zlib')
 const fs = require('fs')
 
-function Xor(key, string){
-      result = '';
-      for (var index = 0; index < string.length; index++) {
-      var input = string.charCodeAt(index);
-      result += String.fromCharCode(input ^ key);
-}
-      return result;
-}
-
-function decodeSave(fileName){
-    data = fs.readFileSync(__dirname + '/' + fileName, "utf-8") //CCGameManager
-    Xord = Xor(0xB, data)
-    base64 = Buffer.from(Xord, 'base64')
-    result = zlib.unzipSync(base64).toString()
-    fs.writeFileSync('decoded.xml', result)
-    return console.log('Your Save Data has been decoded')
+function Xor(key, string) {
+    result = '';
+    for (var index = 0; index < string.length; index++) {
+        var input = string.charCodeAt(index);
+        result += String.fromCharCode(input ^ key);
+    }
+    return result;
 }
 
-function encodeSave(fileName) {
-  data = fs.readFileSync(__dirname + '/' + fileName, "utf8"); //txt, xml, etc...
-  zipped = zlib.gzipSync(data, 'base64')
-  base64 = new Buffer.from(zipped, "utf-8").toString("base64")
-  done = Xor(0xB, base64)
-  fs.writeFileSync('CCGameManager.dat', done)
-  return console.log('Save File Generated')
-  }
+function decodeSave(fileName) {
+    data = fs.readFileSync(__dirname + '/' + fileName, "utf-8") // Reads file and stores it in data
+    Xord = Xor(0xB, data) // applies Xor to it
+    base64 = Xord.replace(/\+/g, '-')
+    base64 = base64.replace(/\//g, '_')
+    base64 = Buffer.from(base64, 'base64') //buffers it from base64
+    result = zlib.gunzipSync(base64).toString() //unzips with Gzip
+    fs.writeFileSync(fileName + ' - decoded.xml', result) //writes a file with the unzipped file
+    return console.log(Xord)
+}
 
-  decodeSave(/*Please insert the name of your save file*/ )
+/*function encodeSave(fileName) {
+    data = fs.readFileSync(__dirname + '/' + fileName, "utf8");
+    zipped = zlib.gzipSync(data, 'base64')
+    base64 = new Buffer.from(zipped, "utf-8").toString('base64')
+    base64 = base64.replace(/-/g, '+')
+    base64 = base64.replace(/_/g, '/')
+    done = Xor(0xB, base64)
+    fs.writeFileSync(fileName + 'New.dat', done)
+    return console.log('Save File Generated')
+}
 
-  encodeSave(/*Please insert the name of the file you want to encode*/ )
+Encoding saves is broken :( will fix soon
+
+*/
+
+decodeSave('CCGameManager.dat')
